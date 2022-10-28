a48c916c8e16a9f82e177eb23a8f86dbdbba90d7
fslon <fedordikyn@gmail.com>
2022-10-28 17:33:10 +0300

    кнопка

--- app/src/main/java/ru/geekbrains/weatherapp/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/weatherapp/MainActivity.kt
@@ -1,15 +1,23 @@
 package ru.geekbrains.weatherapp
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+        addButton()
     }
 
-
+    fun addButton() {
+        val button: Button = findViewById(R.id.testButton)
+        button.text = "test 1"
+        button.setOnClickListener(View.OnClickListener {
+            Toast.makeText(applicationContext, "таыова", Toast.LENGTH_LONG).show()
+        })
+    }
 }
